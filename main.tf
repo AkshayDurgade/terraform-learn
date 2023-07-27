dddd8b2e6a4aa0bf6c8734b17435342c9c571a24
AkshayDurgade <akshay.durgade@mindstix.com>
2023-07-27 12:19:15 +0530

    Ec2 Instance

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@ variable vpc_cidr_block {}
 variable avail_zone {}
 variable env_prefix {}
 variable my_ip {}
+variable instance_type {}
 
 resource "aws_vpc" "myapp-vpc" {
     cidr_block = var.vpc_cidr_block
@@ -137,6 +138,22 @@ data "aws_ami" "latest-amazon-linux-image" {
     }
 }
 
+# resource "aws_key_pair" "ssh-key" {
+#     key_name = "server-key"
+#     public_key = 
+# }
+
 resource "aws_instance" "myapp-server" {
     ami = data.aws_ami.latest-amazon-linux-image.id
+    instance_type = var.instance_type
+    subnet_id = aws_subnet.myapp-subnet-1.id
+    vpc_security_group_ids = [aws_default_security_group.default-sg.id]
+    availability_zone = var.avail_zone   
+
+    associate_public_ip_address = true
+    key_name = "CDAC1"
+
+    tags = {
+        Name = "${var.env_prefix}-server"
+    }
 }
